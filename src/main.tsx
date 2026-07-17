d1de8ebf9dff891f1a58a6c6c145c51399be7508
DOC Carni <doccarni1961@gmail.com>
2026-07-17 21:10:37 +0200

    feat: luxury multipage DOC Carni experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './App'
+import App from './LuxuryApp'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
